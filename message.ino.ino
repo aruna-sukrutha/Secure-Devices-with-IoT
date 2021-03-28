c24901cd16208f8aa5d8fcccc6202451e3dd7959
Aruna Sukrutha Mallampalli <38745074+arunampalli@users.noreply.github.com>
2021-03-27 18:50:34 -0700

    Add files via upload

---
+++ message.ino.ino
@@ -0,0 +1,46 @@
+void setup()
+    {
+    Serial.begin(9600);
+    }
+    void loop()
+    {
+    delay(1200);
+    Serial.print("AT");
+    delay(1200);
+    bool bOK = false;
+    while (Serial.available() > 0)
+    {
+    char inChar = (char)Serial.read();
+    bOK = true;
+    }
+ 
+    if(bOK)
+    {
+    index = 0;
+    Serial.println();
+    Serial.println("AT+CMGF=1"); // sets the SMS mode to text
+    delay(100);
+    delay(1200);
+    bool bOK = false;
+    while (Serial.available() > 0) {
+    //Serial.write(Serial.read());
+    char inChar = (char)Serial.read();
+    bOK = true;
+    }
+    if(bOK)
+    {
+    Serial.println();
+    Serial.print("AT+CMGS=""); // send the SMS number
+    Serial.print("+917358274445");
+    Serial.println(""");
+    delay(500);
+    Serial.print("A new post is created by Zain."); // SMS body
+ 
+    delay(500);
+ 
+    Serial.write(0x1A);
+    Serial.write(0x0D);
+    Serial.write(0x0A);
+    }
+    }
+    }
